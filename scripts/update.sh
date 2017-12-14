7a17b930f13d8921cbe7626e0df535620012eda6
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-15 03:02:14 +0900

    Add auto fetcher

---
+++ scripts/update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+filter_out_ignore(){
+  for i in "$@"
+  do
+    [ 'scripts'  = "$i" ] && continue
+    echo "$i"
+  done
+}
+
+nowdir=`pwd`
+
+cd ..
+
+topdir=`pwd`
+
+git submodule deinit -f .
+git submodule update --init
+
+for workdir in `filter_out_ignore *`
+do
+  cd "$workdir"
+  git fetch origin master
+  git checkout master
+  git rebase origin/master
+  git submodule update --init --recursive
+  cd "$topdir"
+done
+cd "$nowdir"
